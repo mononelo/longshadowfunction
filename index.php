a94090bb9900c695046cc6cbb99485d1820a52ec
mononelo <pol@mononelo.es>
2013-09-16 13:19:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+  <?php 
+  
+  // FUNCTION'S DECLARATION
+  
+  include 'functions.php'; 
+  
+  ?>
+        
+  <div style="<?php text_shadow( $length, $color ); ?>">Lorem Ipsum</div>
+  <div style="<?php box_shadow( $length, $color ); ?>"></div>
